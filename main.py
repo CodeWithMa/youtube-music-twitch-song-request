705564bf103b07b190698e385101523520f4d2be
Ma <101021254+CodeWithMa@users.noreply.github.com>
2025-03-28 19:42:11 +0100

    Move search and queue code to new class YoutubeMusicApiClient and implement authenticate

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from youtube_music_api_client.main import search, queue
+from youtube_music_api_client.youtube_music_api_client import YoutubeMusicApiClient
 
 
 def main():
-    video_id = search("Never gonna")
+    client = YoutubeMusicApiClient()
+    client.authenticate()
+    video_id = client.search("Never gonna")
     if video_id is None:
         return
-    queue(video_id)
+    client.queue(video_id)
 
 
 if __name__ == "__main__":
---
+++ src/libs/youtube-music-api-client/src/youtube_music_api_client/youtube_music_api_client.py
@@ -0,0 +1,72 @@
+import requests
+
+class YoutubeMusicApiClient:
+    def __init__(self):
+        self.api_base_url = "http://localhost:26538"
+        self.api_base_url_v1 = f"{self.api_base_url}/api/v1"
+        self.token = ""
+
+    def authenticate(self):
+        # TODO Use real client id: youtube-music-twitch-song-request
+        client_id = "1"
+        url = f"{self.api_base_url}/auth/{client_id}"
+
+        headers = {
+            "accept": "application/json"
+        }
+        data = {}
+        response = requests.post(url, json=data, headers=headers)
+
+        print("Status: ", response.status_code)
+        #print(response.json())
+
+        if response.status_code != 200:
+            return
+
+        accessToken = response.json()["accessToken"]
+        self.token = accessToken
+
+
+    def search(self, query: str):
+        url = f"{self.api_base_url_v1}/search"
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {self.token}",
+            "Content-Type": "application/json",
+        }
+        data = {"query": query}
+
+        response = requests.post(url, json=data, headers=headers)
+
+        print("Status: ", response.status_code)
+
+        # write to file
+        #with open("search.json", "w") as f:
+        #    f.write(response.text)
+
+        if response.status_code != 200:
+            return None
+            
+        contents = response.json()["contents"]["tabbedSearchResultsRenderer"]["tabs"][0]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]
+        if "musicCardShelfRenderer" in contents:
+            first_search_result = contents["musicCardShelfRenderer"]["title"]["runs"][0]
+            print(f"Adding first search result: {first_search_result['text']}")
+            video_id = first_search_result['navigationEndpoint']['watchEndpoint']['videoId']
+            print(f"Video ID: {video_id}")
+
+            return video_id
+
+        return None
+
+    def queue(self, video_id: str):
+        url = f"{self.api_base_url_v1}/queue"
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {self.token}",
+            "Content-Type": "application/json",
+        }
+        data = {"videoId": video_id, "insertPosition": "INSERT_AT_END"}
+
+        response = requests.post(url, json=data, headers=headers)
+
+        print("Status: ", response.status_code)
